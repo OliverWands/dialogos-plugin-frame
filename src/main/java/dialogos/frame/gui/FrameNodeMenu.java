30295b960f1561c8e746e4f8945bfd90db237470
OliverWands <wandschneider.oliver@gmail.com>
2020-11-08 15:17:19 +0100

    Update: Added the functionality to export Frames to json.

--- src/main/java/dialogos/frame/gui/FrameNodeMenu.java
+++ src/main/java/dialogos/frame/gui/FrameNodeMenu.java
@@ -1,17 +1,12 @@
 package dialogos.frame.gui;
 
-import com.clt.diamant.graph.Node;
-import com.clt.diamant.gui.GraphEditorFactory;
-import com.clt.gui.Images;
 import dialogos.frame.FrameNode;
-import dialogos.frame.FrameGraph;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.util.Collection;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class FrameNodeMenu extends JPanel
 {
@@ -30,6 +25,7 @@ public class FrameNodeMenu extends JPanel
 
         JLabel title = new JLabel("Enter or create a Frame");
         title.setFont(new Font(title.getFont().getName(), Font.BOLD, 14));
+        title.setBorder(new EmptyBorder(0, 0, 20, 0));
 
         constraints.gridx = 0;
         constraints.gridy = 0;
@@ -49,52 +45,51 @@ public class FrameNodeMenu extends JPanel
         createButton.addActionListener(e ->
         {
             JFrame frame = new JFrame();
-            NewFrameDialog frameDialog = new NewFrameDialog(frame, "Create Frame");
-            frameDialog.addWindowListener(new WindowListener()
-            {
-                @Override
-                public void windowOpened(WindowEvent e)
-                {
-
-                }
-
-                @Override
-                public void windowClosing(WindowEvent e)
-                {
-
-                }
-
-                @Override
-                public void windowClosed(WindowEvent e)
-                {
-                    frameNode.frameStruct = frameDialog.getFrameStruct();
-                }
-
-                @Override
-                public void windowIconified(WindowEvent e)
-                {
-
-                }
-
-                @Override
-                public void windowDeiconified(WindowEvent e)
-                {
-
-                }
-
-                @Override
-                public void windowActivated(WindowEvent e)
-                {
-
-                }
-
-                @Override
-                public void windowDeactivated(WindowEvent e)
-                {
-
-                }
-            });
-
+            new NewFrameDialog(frameNode.frameStruct, frame, "Create Frame");
+//            frameDialog.addWindowListener(new WindowListener()
+//            {
+//                @Override
+//                public void windowOpened(WindowEvent e)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void windowClosing(WindowEvent e)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void windowClosed(WindowEvent e)
+//                {
+//                    frameNode.frameStruct = frameDialog.getFrameStruct();
+//                }
+//
+//                @Override
+//                public void windowIconified(WindowEvent e)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void windowDeiconified(WindowEvent e)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void windowActivated(WindowEvent e)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void windowDeactivated(WindowEvent e)
+//                {
+//
+//                }
+//            });
             frame.pack();
             frame.setVisible(true);
         });
@@ -135,6 +130,26 @@ public class FrameNodeMenu extends JPanel
         inputPanel.add(export, constraints);
 
         JButton exportButton = new JButton("Export Frame");
+        exportButton.addActionListener(e ->
+        {
+            JFrame frame = new JFrame();
+            frame.setLayout(new BorderLayout());
+            JFileChooser fileChooser = new JFileChooser();
+
+            frame.add(fileChooser, BorderLayout.SOUTH);
+            if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                File file = fileChooser.getSelectedFile();
+                try (Writer writer = new BufferedWriter(
+                        new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8)))
+                {
+                    writer.write(frameNode.frameStruct.marshal().toString(4));
+                } catch (IOException exp)
+                {
+                    exp.printStackTrace();
+                }
+            }
+        });
 
         constraints.gridx = 1;
         constraints.gridy = 3;
@@ -143,54 +158,6 @@ public class FrameNodeMenu extends JPanel
         constraints.fill = GridBagConstraints.HORIZONTAL;
         inputPanel.add(exportButton, constraints);
 
-        JLabel tagTitle = new JLabel("Load Tags from File");
-        tagTitle.setFont(new Font(tagTitle.getFont().getName(), Font.BOLD, 14));
-
-        constraints.gridx = 0;
-        constraints.gridy = 4;
-        constraints.gridwidth = 1;
-        constraints.anchor = GridBagConstraints.LINE_START;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        inputPanel.add(tagTitle, constraints);
-
-        JButton loadTags = new JButton();
-        loadTags.setIcon(Images.load("OpenFile.png"));
-
-        constraints.gridx = 1;
-        constraints.gridy = 4;
-        constraints.gridwidth = 3;
-        constraints.anchor = GridBagConstraints.LINE_END;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        inputPanel.add(loadTags, constraints);
-
-        //
-        // TODO for testing purposes
-        //
-        JButton showGraph = new JButton("Show Graph");
-        showGraph.addActionListener(e -> showGraph());
-
-        constraints.gridx = 1;
-        constraints.gridy = 5;
-        constraints.gridwidth = 3;
-        constraints.anchor = GridBagConstraints.LINE_END;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        inputPanel.add(showGraph, constraints);
-
         add(inputPanel, BorderLayout.CENTER);
-    }
-
-    private void showGraph()
-    {
-        GraphEditorFactory.show(frameNode);
-        Collection<Node> mainGraphNodes = frameNode.getGraph().getNodes();
-        for (Node node : mainGraphNodes)
-        {
-            if (node.getClassName().equals(FrameNode.class.getName()))
-            {
-                FrameNode fNode = (FrameNode) node;
-                FrameGraph frameGraphBuilder = new FrameGraph(fNode);
-                frameGraphBuilder.buildGraph();
-            }
-        }
     }
 }
